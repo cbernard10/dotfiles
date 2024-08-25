dfe82848102c685e23ef174096be274ec2f6bada
cbernard10 <clement.bernard@protonmail.com>
2024-08-25 23:17:02 +0200

    weekly update

--- fetch.sh
+++ fetch.sh
@@ -1,16 +1,16 @@
 cp ~/.config/alacritty/ .
-cp ~/.config/betterlockscreen/
-cp ~/.config/bspwm/
-cp ~/.config/btop
-cp ~/.config/cava/
-cp ~/.config/neofetch/
-cp ~/.config/nvim/
-cp ~/.config/picom/
-cp ~/.config/polybar/
-cp ~/.config/ranger/
-cp ~/.config/sxhkd/
-cp ~/.Xresources
-cp ~/.xinitrc
+cp ~/.config/betterlockscreen/ .
+cp ~/.config/bspwm/ .
+cp ~/.config/btop .
+cp ~/.config/cava/ . 
+cp ~/.config/neofetch/ .
+cp ~/.config/nvim/ . 
+cp ~/.config/picom/ .
+cp ~/.config/polybar/ .
+cp ~/.config/ranger/ .
+cp ~/.config/sxhkd/ .
+cp ~/.Xresources .
+cp ~/.xinitrc .
 git add .
 git commit -m "weekly update"
 git push 
