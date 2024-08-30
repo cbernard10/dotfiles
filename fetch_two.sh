5d408d0c41071583c6d77f5f4009832f1432423f
cbernard10 <clement.bernard@protonmail.com>
2024-08-30 11:04:27 +0200

    refactoring

---
+++ fetch_two.sh
@@ -0,0 +1,17 @@
+git pull
+cp -r ~/.config/alacritty/ two/
+cp -r ~/.config/betterlockscreen/ two/
+cp -r ~/.config/bspwm/ two/
+cp -r ~/.config/btop two/
+cp -r ~/.config/cava/ two/
+cp -r ~/.config/neofetch/ two/
+cp -r ~/.config/nvim/ two/
+cp -r ~/.config/picom/ two/
+cp -r ~/.config/polybar/ two/
+cp -r ~/.config/ranger/ two/
+cp -r ~/.config/sxhkd/ two/
+cp ~/.Xresources two/
+cp ~/.xinitrc two/
+git add .
+git commit -m "update two"
+git push 
