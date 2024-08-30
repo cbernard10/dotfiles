456e837414723ebb794b0df887bbead116e24c76
cbernard10 <clement.bernard@protonmail.com>
2024-08-30 11:01:16 +0200

    update one

---
+++ fetch_one.sh
@@ -0,0 +1,17 @@
+git pull
+cp -r ~/.config/alacritty/ one/
+cp -r ~/.config/betterlockscreen/ one/
+cp -r ~/.config/bspwm/ one/
+cp -r ~/.config/btop one/
+cp -r ~/.config/cava/ one/
+cp -r ~/.config/neofetch/ one/
+cp -r ~/.config/nvim/ one/
+cp -r ~/.config/picom/ one/
+cp -r ~/.config/polybar/ one/
+cp -r ~/.config/ranger/ one/
+cp -r ~/.config/sxhkd/ one/
+cp ~/.Xresources one/
+cp ~/.xinitrc one/
+git add .
+git commit -m "update one"
+git push 
